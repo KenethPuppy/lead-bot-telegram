b152f762e64bdfd7db997ae2b082d2d34530846a
KenethPuppy <kenethpuppy@gmail.com>
2024-01-30 01:26:51 +0300

    Test

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,103 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+import { Bot, Keyboard, Context, session, SessionFlavor } from "grammy";
+import {
+  type Conversation,
+  type ConversationFlavor,
+  conversations,
+  createConversation,
+} from "@grammyjs/conversations";
+import * as dotenv from 'dotenv'
+import { Bitrix } from '@2bad/bitrix'
+import { freeStorage } from "@grammyjs/storage-free";
+import { SessionData } from "./types.js";
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({
+  "path": __dirname + '/.env'
+})
+
+const bitrix = Bitrix(process.env.WEBHOOK_BITRIX)
+
+type MyContext = Context & SessionFlavor<SessionData> & ConversationFlavor;
+type MyConversation = Conversation<MyContext>;
+
+const bot = new Bot<MyContext>(process.env.BOT_TOKEN)
+
+bot.use(session({
+  "storage": freeStorage<SessionData>(bot.token),
+  initial: () => ({  }),
+}));
+
+
+async function leadProcess(conversation: MyConversation, ctx: MyContext): Promise<void> {
+  await ctx.reply('Какой документ вам необходимо оформить?', {
+    "reply_markup": { remove_keyboard: true },
+  })
+  do {
+    ctx = await conversation.waitFor("message:text");
+  } while (!ctx.message.text)
+  ctx.session.text = ctx.message.text
+  do {
+    const contactBtn = new Keyboard().requestContact('Поделиться контактом').resized()
+    await ctx.reply(`Поделитесь вашим контактом, чтобы я мог с вами связаться и предложить лучшую цену на рынке!`, {
+      "reply_markup": contactBtn
+    })
+    ctx = await conversation.waitFor("message:contact")
+  } while (!ctx.message.contact)
+  ctx.session.phoneNumber = ctx.message.contact.phone_number
+  conversation.log(ctx.session)
+  const resultQuery = await conversation.external(async () => await sendDataBitrix(ctx.session))
+  if (resultQuery) {
+    const keyboard = new Keyboard().text('Оформить сертификат').resized().persistent();
+    await ctx.reply(`Спасибо, что оставили ваш контакт. Уже бегу за телефоном, чтобы вам позвонить!`, {
+      "reply_markup": keyboard
+  }) 
+  } else {
+    await ctx.reply(`Произошла ошибка! Попробуйте ещё раз!`)
+  }
 }
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
+
+const sendDataBitrix = async ({ name, phoneNumber, text, code }: SessionData): Promise<boolean> => {
+  try {
+    const bitrixResult = await bitrix.leads.create({
+      "NAME": name,
+      "PHONE": [ { "VALUE": phoneNumber, "VALUE_TYPE": "MOBILE" } ],
+      "COMMENTS": text,
+      "TITLE": code,
+      "SOURCE_ID": "чат-бот"
+    })
+    console.log(bitrixResult)
+    return true
+  } catch (error) {
+    console.log("Произошла ошибка!")
+    console.log(error)
+    return false
+  } 
 }
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
-}
+bot.use(conversations());
+bot.use(createConversation(leadProcess));
+
+
+bot.command('start', async (ctx) => {
+  const codeLink = ctx.match;
+  ctx.session.code = codeLink
+  ctx.session.telegramId = ctx.from.id.toString()
+  ctx.session.name = ctx.from.last_name? ctx.from.first_name +'' + ctx.from.last_name : ctx.from.first_name
+  const greetText = `Здравствуйте, ${ctx.session.name}! \nМеня зовут Владимир Филатов, готов оформить для вас любой сертификат от аккредитованной лаборатории. `
+  await ctx.reply(greetText)
+  await ctx.conversation.enter("leadProcess")
+})
+
+
+bot.hears('Оформить сертификат', async (ctx) => {
+  await ctx.conversation.enter("leadProcess")
+})
+
+
+bot.start()
+
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export interface SessionData {
+    code?: string;
+    telegramId?: string;
+    phoneNumber?: string;
+    name?: string;
+    text?: string;
+  }
